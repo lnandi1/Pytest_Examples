8186e6b3e2521a71cb45f5228e2fb7920c4a0746
Sarunas Azna <sarunas.azna@gmail.com>
2019-07-13 14:24:34 +0200

    add dummy source code test

--- test_Program1.py
+++ test_Program1.py
@@ -1,9 +1,15 @@
+import inspect
+
 def the_function(x, y):
     return x*y
 
 
-def test_beep():
-    x = 1
+def test_math():
+    x = 2
     y = 3
     expected = 6
     assert the_function(x, y) == expected
+
+def test_source():
+    source_code = inspect.getsource(the_function)
+    assert 'range(' in source_code
